7c2e3fc12f83cedf05d976b1914917c617e8bc42
Aldo Utrera <aldo.utrera@travelogyx.com>
2016-04-11 14:03:23 -0500

    Estilizando el formulario de reservacion

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,6 @@
 class ReservationsController < ApplicationController
   def new
+    hotel_rechecked_rates = File.read(Rails.root + "app/assets/jsons/recheck_rate.json")
+    @hotel = (JSON.parse hotel_rechecked_rates)['hotel']
   end
 end
