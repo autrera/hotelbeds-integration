8e11de1bd1a72a7096b9f16322934ea670445fc8
Aldo Utrera <aldo.utrera@travelogyx.com>
2016-06-17 14:03:46 -0500

    Removiendo ruta del certificado

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/.well-known/acme-challenge/:id' => 'welcome#letsencrypt'
+  # get '/.well-known/acme-challenge/:id' => 'welcome#letsencrypt'
 
   devise_for :agents
   devise_for :clients
